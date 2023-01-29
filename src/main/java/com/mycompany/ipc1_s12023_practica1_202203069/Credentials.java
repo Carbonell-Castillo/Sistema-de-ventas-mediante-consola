e167827c487926fc335432ce56baf1d669a79b45
Carbonell-Castillo <carbonellcastillo1204@gmail.com>
2023-01-28 23:01:35 -0600

    Add fundamental data

---
+++ src/main/java/com/mycompany/ipc1_s12023_practica1_202203069/Credentials.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ipc1_s12023_practica1_202203069;
+
+/**
+ *
+ * @author bruce
+ */
+public class Credentials {
+    private String user;
+    private String password;
+
+    public Credentials(){
+        user="bruce";
+        password="123";
+    }
+    
+    public Credentials(String user, String password) {
+        this.user = user;
+        this.password = password;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ src/main/java/com/mycompany/ipc1_s12023_practica1_202203069/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ipc1_s12023_practica1_202203069;
+
+/**
+ *
+ * @author bruce
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        Credentials ejecutar= new Credentials();
+        
+        System.out.println("Usuario "+ejecutar.getUser());
+        System.out.println("Password "+ejecutar.getPassword());
+    }
+}
